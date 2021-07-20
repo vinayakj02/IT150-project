0159a1ff10863979e020258a7b54da9fcc4aebab
Vinayak <vinayakskye@gmail.com>
2021-07-20 12:08:11 +0530

    TSLA

--- ParsingJSON-1/Main.java
+++ ParsingJSON-1/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) throws JSONException {
 	// write your code here
-    StockData TSLA = new StockData("FB");
+    StockData TSLA = new StockData("TSLA");
         System.out.println(TSLA.getOpenPrice());
     }
 }
