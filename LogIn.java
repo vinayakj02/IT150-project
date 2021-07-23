97ae3c14fd1c946b5ba6850bb7cce430aaffece5
Nikhil-Satish <77330133+Nikhil-Satish@users.noreply.github.com>
2021-07-23 17:04:08 +0530

    Updated LogIn class and USERDATA.json

--- LogIn.java
+++ LogIn.java
@@ -4,6 +4,8 @@ import org.json.JSONObject;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Iterator;
 import java.util.Scanner;
 
@@ -37,12 +39,33 @@ public class LogIn {
             }
         }
         return false;
+//        String.valueOf(0)
     }
+    
+    public void SignUp(Account acc) throws JSONException {
+    	JSONObject ID1 = new JSONObject();
+	      //Inserting key-value pairs into the json object
+	      ID1.put("userName", acc.getUserName());
+	      ID1.put("password", acc.getPassword());
+		  ID1.put("balance", String.valueOf(acc.getBalance()));
+		  userInfo.accumulate("abc12", ID1);
+		  try {
+		         FileWriter file = new FileWriter("USERDATA.json");
+//		         file.write(jsonObject.toJSONString());
+		         file.write(userInfo.toString());
+		         file.close();
+		      } catch (IOException e) {
+		         // TODO Auto-generated catch block
+		         e.printStackTrace();
+		      }
+		      System.out.println("JSON file created: "+userInfo.toString(2));
+	}
 
     public static void main(String[] args) throws FileNotFoundException, JSONException {
-        Account bukcy = new Account("ID2","loki","secretpassword",2001);
+        Account bukcy = new Account("ID3","random","secretpassword",2001);
         LogIn l1 = new LogIn();
 //        System.out.println(l1.AccountExists(bukcy));
-        System.out.println(l1.SignIN(bukcy));
+//        System.out.println(l1.SignIN(bukcy));
+        l1.SignUp(bukcy);
     }
 }
